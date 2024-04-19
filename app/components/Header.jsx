370194dd03b987b19d66fc3f36562f772a3c1767
Sam Gubernikoff <sgubernikoff@gmail.com>
2024-04-19 11:00:11 -0400

    no scroll

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -22,6 +22,7 @@ export function Header({header, isLoggedIn, cart, supportMenu, mobileMenu}) {
   const [isOpen, setIsOpen] = useState(false);
   function toggleMenu() {
     setIsOpen(!isOpen);
+    document.body.classList.toggle('no-scroll', !isOpen);
   }
 
   const {shop, menu} = header;
