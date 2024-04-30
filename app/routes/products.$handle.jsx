f62b5251e6770ada5672fc0a650a26bae88e2122
alexander gubernikoff <agubernikoff@gmail.com>
2024-04-30 13:19:09 -0400

    mobile pdp image swipe

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -186,6 +186,28 @@ function ProductImage({images, selectedVariant, isMobile}) {
     ></div>
   ));
 
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isRightSwipe) cycleImages(-1);
+    if (isLeftSwipe) cycleImages(1);
+  };
+
   if (!images) {
     return (
       <div className="product-image">
@@ -207,12 +229,18 @@ function ProductImage({images, selectedVariant, isMobile}) {
             onClick={() => {
               cycleImages(-1);
             }}
+            onTouchStart={onTouchStart}
+            onTouchMove={onTouchMove}
+            onTouchEnd={onTouchEnd}
           />
           <div
             className="right-image-button-container"
             onClick={() => {
               cycleImages(1);
             }}
+            onTouchStart={onTouchStart}
+            onTouchMove={onTouchMove}
+            onTouchEnd={onTouchEnd}
           />
           <div
             style={{
