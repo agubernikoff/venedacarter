98a620a350e0e13aaf26ff74dfc1cdd489f57f56
Sam Gubernikoff <sgubernikoff@gmail.com>
2024-05-21 12:14:51 -0400

    color picker

--- app/helper/ColorPicker.js
+++ app/helper/ColorPicker.js
@@ -1,4 +1,6 @@
 export default function colorPicker(material) {
-  if (material.toLowerCase().includes('silver')) return 'silver';
-  if (material.toLowerCase().includes('gold')) return 'gold';
+  if (material.toLowerCase().includes('silver')) return '#c0c0c0';
+  if (material.toLowerCase().includes('yellow')) return '#E5E4E2';
+  if (material.toLowerCase().includes('white')) return '#ffd700';
+  if (material.toLowerCase().includes('gold')) return '#d4af37';
 }
