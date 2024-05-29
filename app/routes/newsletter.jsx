35763bbd7548765668ab227c7d195fb4b5e9c3ef
Sam Gubernikoff <sgubernikoff@gmail.com>
2024-05-29 15:36:24 -0400

    newsletter page

---
+++ app/routes/newsletter.jsx
@@ -0,0 +1,49 @@
+import {redirect, json} from '@shopify/remix-oxygen';
+import {
+  Form,
+  NavLink,
+  useActionData,
+  useNavigate,
+  useNavigation,
+  useOutletContext,
+} from '@remix-run/react';
+import {CUSTOMER_LOGIN_MUTATION} from '../graphql/customer-account/CustomerLogin';
+import {useEffect} from 'react';
+
+/**
+ * @type {MetaFunction}
+ */
+export const meta = () => {
+  return [{title: 'Newsletter'}];
+};
+
+export default function AccountProfile() {
+  const account = useOutletContext();
+  const {state} = useNavigation();
+  /** @type {ActionReturnData} */
+  const action = useActionData();
+
+  return (
+    <div className="account-login">
+      <p className="stockists-title">NEWSLETTER</p>
+      <div className="newsletter-mobile-page">
+        <div class="klaviyo-form-XrMRY4"></div>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * @typedef {{
+ *   error: string | null;
+ *   customer: CustomerFragment | null;
+ * }} ActionResponse
+ */
+
+/** @typedef {import('customer-accountapi.generated').CustomerFragment} CustomerFragment */
+/** @typedef {import('@shopify/hydrogen/customer-account-api-types').CustomerUpdateInput} CustomerUpdateInput */
+/** @typedef {import('@shopify/remix-oxygen').ActionFunctionArgs} ActionFunctionArgs */
+/** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
+/** @template T @typedef {import('@remix-run/react').MetaFunction<T>} MetaFunction */
+/** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof loader>} LoaderReturnData */
+/** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof action>} ActionReturnData */
