feac1e0fbdb6d1e4fcc3e03e389b85dc770424ba
Sam Gubernikoff <sgubernikoff@gmail.com>
2024-05-30 11:36:04 -0400

    recover not reset

--- app/graphql/customer-account/CustomerResetByUrl.js
+++ app/graphql/customer-account/CustomerResetByUrl.js
@@ -1,5 +1,5 @@
 // app/graphql/customer-account/CustomerResetByUrl.js
-export const CUSTOMER_RESET_BY_URL_MUTATION = `#graphql
+export const CUSTOMER_RECOVER_BY_URL_MUTATION = `#graphql
 mutation customerResetByUrl($email: String!) {
   customerRecover(email: $email) {
     customerUserErrors {
