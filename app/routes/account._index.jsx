e12e110e4d3ee407fb1dce96c8e55fb40655b3d5
Sam Gubernikoff <sgubernikoff@gmail.com>
2024-05-30 11:22:20 -0400

    account_index loader update

--- app/routes/account._index.jsx
+++ app/routes/account._index.jsx
@@ -1,9 +1,15 @@
 import {redirect} from '@shopify/remix-oxygen';
 
 export async function loader({context}) {
-  if (context.session.get('customerAccessToken'))
-    return redirect('/account/profile');
-  else return redirect('/account/login');
+  const currentURL = context.req.url;
+
+  if (!currentURL.includes('/account/reset')) {
+    if (context.session.get('customerAccessToken'))
+      return redirect('/account/profile');
+    else return redirect('/account/login');
+  } else {
+    return json({});
+  }
 }
 
 /** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof loader>} LoaderReturnData */
